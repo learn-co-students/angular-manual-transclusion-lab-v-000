3b2a8e986b22ac36372f77e925da9d0d79cf6c76
onyoo <roberto9m9@gmail.com>
2016-03-19 10:57:05 +0100

    Done.

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -2,11 +2,16 @@ function UserProfile() {
 	return {
 		transclude: true,
 		template: [
-			'<div>',
+			'<div style="border: 1px solid black;"">',
 				'<h3>User Profile</h3>',
-				'<span></span>',
+				'This is part of the template',
+				'This is from the directive in the index page:<span></span>',
+				"notice we didn't overwrite it",
 			'</div>'
-		].join('')
+		].join(''),
+		link: function(scope, element, attrs, ctrl, transclude) {
+			element.find('span').after(transclude());
+		}
 	}
 }
 
