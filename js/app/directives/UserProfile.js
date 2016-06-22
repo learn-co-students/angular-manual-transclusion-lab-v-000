740802dd5a7a9c39bd2d2762315bb55f448b4dcb
Brad1974 <danieltbrad@gmail.com>
2016-06-21 20:40:35 -0700

    Done.

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -6,9 +6,12 @@ function UserProfile() {
 				'<h3>User Profile</h3>',
 				'<span></span>',
 			'</div>'
-		].join('')
-	}
-}
+		].join(''),
+		link: function (scope, element, attrs, ctrl, transclude) {
+		    element.find('span').after(transclude());
+		    	}
+		  	};
+			}
 
 angular
 	.module('app')
